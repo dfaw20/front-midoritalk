6cb2dfe3e14b4ccde3be6f305e5d7474309b53f9
umanoto <bumanoton@gmail.com>
2023-02-06 22:48:10 +0900

    main

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Character} from "../domain/entity";
 
 function CharacterList() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+
   return (
     <>
-
     </>
   );
 }
