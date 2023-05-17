956dbc88a0b82c429c33a1fc28629ceadb0c51b1
umanoto <bumanoton@gmail.com>
2023-05-18 04:39:43 +0900

    app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,17 +1,61 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const StyledUlHeader = styled.header`
-  background-color: #92DA57;
-  height:50px;
-`
+import {AppBar, Box, IconButton, Menu, MenuItem, Toolbar} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 function AppHeader(): JSX.Element {
 
-  return (
-    <StyledUlHeader>
-    </StyledUlHeader>
-  )
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>): void => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (): void => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar sx={{justifyContent: "space-between"}}>
+                    <div/>
+
+                    <div>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="inherit"
+                            edge="start"
+                            sx={{ml: 2}}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose}>Account</MenuItem>
+                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
 
 export default AppHeader
